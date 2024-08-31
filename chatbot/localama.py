3f52b12b544884ad270283e6e42a99ba85ae8860
Kunal-ahuja-88 <kahujamp4@gmail.com>
2024-08-31 22:39:00 +0530

    making optimations

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -1,37 +1,44 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.propmpts import ChatPromptTemplate
-from langchain_core.output import ChatOutputTemplate
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
 
 import streamlit as st
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+# Set environment variables
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+api_key = os.getenv("LANGCHAIN_API_KEY")
+if api_key:
+    os.environ["LANGCHAIN_API_KEY"] = api_key
+else:
+    raise EnvironmentError("LANGCHAIN_API_KEY is not set in the environment variables.")
 
 # Prompt Template
-
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant . Please response to the queries")
-        ("user","Question : {question}")
+        ("system", "You are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question: {question}")
     ]
 )
 
-#streamlit framework
-
+# Streamlit framework
 st.title('Langchain Demo With LLAMA2 API')
-input_text=st.text_input("Search the topic u want")
+input_text = st.text_input("Search the topic you want")
 
-# ollama LLAma2 LLm 
-llm=Ollama(model="llama2")
+# Ollama Llama2 LLM 
+llm = Ollama(model="llama2")
 output_parser = StrOutputParser()
-chain=prompt|llm|output_parser
+chain = prompt | llm | output_parser
 
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
+    result = chain.invoke({"question": input_text})
+    st.write(result)
+
+
 
 
